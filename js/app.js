88c65cea81bcd8bffd2a46c5721cd51ab5d8006b
rurabe <rurabe@gmail.com>
2012-08-05 22:00:04 -0700

    finished 2nd part, added routing and filtering

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function ($) {
  
     var contacts = [
-        { name: "Brett", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
         { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
         { name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
         { name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
@@ -40,9 +40,14 @@
         initialize: function () {
             this.collection = new Directory(contacts);
             this.render();
+            this.$el.find("#filter").append(this.createSelect());
+            this.on("change:filterType", this.filterByType, this);
+            this.collection.on("reset",this.render,this)
         },
      
         render: function () {
+            this.$el.find("article").remove();
+
             var that = this;
             _.each(this.collection.models, function (item) {
                 that.renderContact(item);
@@ -54,10 +59,71 @@
                 model: item
             });
             this.$el.append(contactView.render().el);
+        },
+
+        getTypes: function(){
+            return _.uniq(this.collection.pluck("type"));
+        },
+
+        createSelect: function() {
+
+            var select = $("<select/>", {
+                html: "<option value='all'>All</option>"
+            });
+            _.each(this.getTypes(), function(item){
+                var option = $("<option/>", {
+                    value: item,
+                    text: item
+                }).appendTo(select);
+            });
+        return select;
+        },
+
+        events: {
+            "change #filter select": "setFilter"
+        },
+
+        setFilter: function (e) {
+            this.filterType = e.currentTarget.value;
+            this.trigger("change:filterType");
+        },
+
+        filterByType: function () {
+            if (this.filterType === "all") {
+                this.collection.reset(contacts);
+                contactsRouter.navigate("filter/all");
+            } else {
+                this.collection.reset(contacts, { silent: true });
+         
+                var filterType = this.filterType,
+                    filtered = _.filter(this.collection.models, function (item) {
+                        return item.get("type") === filterType;
+                    });
+         
+                this.collection.reset(filtered);
+
+                contactsRouter.navigate("filter/" + filterType);
+            }
         }
     });
 
+    var ContactsRouter = Backbone.Router.extend({
+        routes: {
+            "filter/:type": "urlFilter"
+        },
+
+        urlFilter: function (type) {
+            directory.filterType = type;
+            directory.trigger("change:filterType");
+        }
+    });
+
+
     var directory = new DirectoryView();
+
+    var contactsRouter = new ContactsRouter();
+
+    Backbone.history.start();
 
 } (jQuery));
 
